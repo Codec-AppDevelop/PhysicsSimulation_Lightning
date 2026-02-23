fe426eefc7337d106e3e00c9e29e367d164e644f
Codec <c13o42d98e00c87@gmail.com>
2026-02-23 20:16:20 +0900

    Added outline

--- Lightning_figure.py
+++ Lightning_figure.py
@@ -4,19 +4,48 @@ import matplotlib.pyplot as plt
 
 def create_polylineFig(data):
 
-  dataNp = np.insert(data, 2, np.linspace(0, data.shape[0]-1, data.shape[0]), axis=1) # idx
-  dataNp = np.insert(dataNp, 3, 0, axis=1) # trace
+  dataList = data.tolist()
+  dataList = [[i_data[0], i_data[1], idx, []] for idx, i_data in enumerate(dataList)]
+  # dataNp = np.insert(data, 2, np.linspace(0, data.shape[0]-1, data.shape[0]), axis=1) # idx
 
-  for idx, i_data in enumerate(dataNp):
+  currentTrace = 0
+  for idx, i_data in enumerate(dataList):
 
     # center grid
     grid = i_data[0:1]
+    currentIdx = dataList[2]
 
     # surrounding grid
     surroundGrid = []
     if grid[0] != 0:
       surroundGrid.append([i_data[0]-1, i_data[1]])
+    if grid[0] != 0:
+      surroundGrid.append([i_data[0], i_data[1]-1])
+    surroundGrid.append([i_data[0]+1, i_data[1]])
+    surroundGrid.append([i_data[0], i_data[1]+1])
 
+    # judge
+    # continuous if maximum index within grids whose order is less than the grid
+    contFlg = False
+
+    idxList = []
+    maxTraceIdx = 0
+    for i_surroundGrid in surroundGrid:
+      if i_surroundGrid in dataList:
+        idxTmp = dataList.index(i_surroundGrid)
+        idxList.append(idxTmp)
+
+        if idxTmp == currentIdx - 1:
+          contFlg = True
+        elif idxTmp < currentIdx:
+          maxTraceIdx = np.max([maxTraceIdx, idxTmp])
+
+    # process
+    if contFlg == True:
+      dataList[idx][3].append(currentTrace)
+    else:
+      currentTrace = currentTrace + 1
+      dataList[idx][3].append(currentTrace)
 
 
 
